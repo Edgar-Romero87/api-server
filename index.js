f772012319b89f2aeb062706562324a27a5c93b1
EDGAR ROMERO <EDGAROMERO1987@GMAIL.COM>
2020-09-05 19:34:41 -0700

    server is conecting after adding schema and collection files

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict';
 
-// const mongoose = require('mongoose');
-// require('dotenv').config();
+const mongoose = require('mongoose');
 
 const server = require('./lib/server');
 require('dotenv').config();
 
-server.start(process.env.PORT);
+const MONGODB_URI = 'mongodb://localhost:27017/class-08';
 
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
+//server.start(process.env.PORT);
 
-// })
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions => {
+  
+  server.start(3000);
+
+});
